dabf068ebc881967048467f82d3b0fbff82034b6
vdcn1 <veetsirrah@gmail.com>
2021-11-10 12:28:01 -0300

    fixing boto3 Instance constructor failure

--- http-server/app.py
+++ http-server/app.py
@@ -11,12 +11,18 @@ app = Flask(__name__)
 @app.route("/tags")
 def retrieve_tags():
     ec2 = boto3.client('ec2', region_name="us-east-2")
-    instance = ec2.Instance(ec2_metadata.instance_id)
-    return instance.tags
+    print(ec2_metadata.instance_id)
+    response = ec2.describe_instances(InstanceIds=[ec2_metadata.instance_id])
+    for reservation in response["Reservations"]:
+        for instance in reservation["Instances"]:
+            for tags in instance["Tags"]:
+                res = []
+                res.append(tags)
+    return res
 
 @app.route("/shutdown")
 def shutdown_instance():
     ec2 = boto3.client('ec2', region_name="us-east-2")
-    current_instance = ec2_metadata.instance_id
+    current_instance = ec2_metadata.instance_id 
     ec2.stop_instances(InstanceIds=[str(current_instance)])
-    return "Shuting down..."
+    return "Shuting down...\n"
--- test/terraform_hello_world_example_test.go
+++ test/terraform_hello_world_example_test.go
@@ -1,14 +1,14 @@
 package test
 
 import (
+	"fmt"
 	"github.com/gruntwork-io/terratest/modules/aws"
 	"github.com/gruntwork-io/terratest/modules/terraform"
 	"github.com/stretchr/testify/assert"
 	"io/ioutil"
-	"fmt"
-	"time"
 	"net/http"
 	"testing"
+	"time"
 )
 
 func TestTerraformHelloWorldExample(t *testing.T) {
@@ -20,12 +20,6 @@ func TestTerraformHelloWorldExample(t *testing.T) {
 	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
 		// Set the path to the Terraform code that will be tested.
 		TerraformDir: "../instances",
-
-		// Add tags so terraform can assign them to instance and s3
-		Vars: map[string]interface{}{
-			"name_tag": "Flugel",
-			"owner_tag": "InfraTeam",
-		},
 	})
 
 	// Clean up resources with "terraform destroy" at the end of the test.
@@ -34,11 +28,11 @@ func TestTerraformHelloWorldExample(t *testing.T) {
 	// Run "terraform init" and "terraform apply". Fail the test if there are any errors.
 	terraform.InitAndApply(t, terraformOptions)
 
-	// Run `terraform output` to get the values of output variables and check they have the expected values.	
+	// Run `terraform output` to get the values of output variables and check they have the expected values.
 	instance_id := terraform.Output(t, terraformOptions, "instance_id")
-	
+
 	tags := aws.GetTagsForEc2Instance(t, awsRegion, instance_id)
-	
+
 	var name_compare = "Flugel"
 	var owner_compare = "InfraTeam"
 
@@ -49,32 +43,30 @@ func TestTerraformHelloWorldExample(t *testing.T) {
 	fmt.Println(instanceURLTags)
 
 	// Specify the text the EC2 Instance will return when we make HTTP requests to it.
-	instanceText := fmt.Sprintf("[{\"Key\": \"Name\", \"Value\": \"%s\"}, {\"Key\": \"Owner\", \"Value\": \"%s\"}]", tags["Name"], tags["Owner"])
+	instanceText := fmt.Sprintf("[{\"Key\": \"Owner\", \"Value\": \"%s\"}, {\"Key\": \"Name\", \"Value\": \"%s\"}]", tags["Owner"], tags["Name"])
 
 	// It can take a minute or so for the Instance to boot up, so retry a few times: (waiting a few seconds)
 	time.Sleep(100 * time.Second)
-	
+
 	resp, err := http.Get(instanceURLTags)
-	
+
 	if err != nil {
 		fmt.Println(err)
 	}
-	
+
 	body, _ := ioutil.ReadAll(resp.Body)
 	fmt.Println(string(body))
 	assert.Equal(t, instanceText, string(body))
 
 	resp, err = http.Get(instanceURLShutdown)
-	
+
 	if err != nil {
 		fmt.Println(err)
 	}
-	
+
 	body, _ = ioutil.ReadAll(resp.Body)
 	fmt.Println(string(body))
 	assert.Equal(t, "Shuting down...", string(body))
-	
-	
 
 	assert.Equal(t, name_compare, tags["Name"])
 	assert.Equal(t, owner_compare, tags["Owner"])
